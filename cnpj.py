363048a5c6b64d480fbf97baca546660a21bccd4
Carlos Alberto <devcarlosalberto@gmail.com>
2022-04-18 10:37:30 -0300

    first commit

---
+++ cnpj.py
@@ -0,0 +1,83 @@
+"""
+04.252.011/0001-10
+40.688.134/0001-61
+71.506.168/0001-11
+12.544.992/0001-05
+
+0   4   2   5   2   0   1   1   0   0   0   1   X   X
+5   4   3   2   9   8   7   6   5   4   3   2
+0   16  6   10  18  0   7   6   0   0   0   2 = 65
+Fórmula -> 11 - (65 % 11) = 1
+Primeiro digito = 1 (Se o digito for maior que 9, ele se torna 0)
+
+0   4   2   5   2   0   1   1   0   0   0   1   1   X
+6   5   4   3   2   9   8   7   6   5   4   3   2
+0   20  8   15  4   0   8   7   0   0   0   3   2 = 67
+Fórmula -> 11 - (67 % 11) = 10 (Como o resultado é maior que 9, então é 0)
+Segundo digito = 0
+
+Novo CNPJ + Digitos = 04.252.011/0001-10
+CNPJ Original =       04.252.011/0001-10
+Válido
+
+Recap.
+543298765432 -> Primeiro digito
+6543298765432 -> Segundo digito
+"""
+
+REGRESSIVOS = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+
+
+def formatar(cnpj):
+    return str(cnpj).replace('.', '').replace('/', '').replace('-', '').replace(' ', '')
+
+
+def eh_sequencia(cnpj):
+    sequencia = cnpj[0] * len(cnpj)
+    if sequencia == cnpj:
+        return True
+    else:
+        return False
+
+
+def calcular_digito(cnpj, digito):
+
+    if digito == 1:
+        multiplicadores = REGRESSIVOS[1:]
+        novo_cnpj = cnpj[:-2]
+    elif digito == 2:
+        multiplicadores = REGRESSIVOS
+        novo_cnpj = cnpj
+    else:
+        return None
+
+    total = 0
+    for indice, multiplicador in enumerate(multiplicadores):
+        total += int(cnpj[indice]) * multiplicador
+
+    digito = 11 - (total % 11)
+    if digito < 9:
+        novo_cnpj += str(digito)
+    else:
+        novo_cnpj += '0'
+
+    return novo_cnpj
+
+
+def validar(cnpj):
+    cnpj = formatar(cnpj)
+
+    if eh_sequencia(cnpj):
+        return False
+
+    novo_cnpj = calcular_digito(cnpj=cnpj, digito=1)
+    novo_cnpj = calcular_digito(cnpj=novo_cnpj, digito=2)
+
+    if novo_cnpj == cnpj:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    validar('04.252.011/0001-10')
